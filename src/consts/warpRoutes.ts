34e6bd661daa42e9bada3d19aa58a7facc48700e
Ayrtonmanuel <ayrtonmanuel@gmail.com>
2025-01-07 06:05:15 -0400

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,5 +1,5 @@
 tokens: [
-     
+     {
        chainName: base
         standard: EvmHypNative
         decimals: 18
@@ -11,8 +11,8 @@ tokens: [
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
           - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
-            ,
-    
+            },
+    {
        chainName: lisk
         standard: EvmHypNative
         decimals: 18
@@ -24,8 +24,8 @@ tokens: [
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
           - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
-            ,
-    
+            },
+    {
        chainName: mint
         standard: EvmHypNative
         decimals: 18
@@ -37,8 +37,8 @@ tokens: [
           - token: ethereum|lisk|0x274c9f6ac5bD2Be638e8122e8C656a9899B60cc8
           - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
-            ,
-    
+            },
+    {
        chainName: optimism
         standard: EvmHypNative
         decimals: 18
@@ -50,8 +50,8 @@ tokens: [
           - token: ethereum|lisk|0x274c9f6ac5bD2Be638e8122e8C656a9899B60cc8
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
-            ,
-    
+            },
+    {
        chainName: swell
         standard: EvmHypCollateral
         decimals: 18
@@ -64,6 +64,6 @@ tokens: [
           - token: ethereum|lisk|0x274c9f6ac5bD2Be638e8122e8C656a9899B60cc8
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
           - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
-    
+   } 
 ]
 options: {}
