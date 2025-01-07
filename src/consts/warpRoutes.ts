8b5e036665b2b9af182e6a8b1c1fb26eae940d22
Ayrtonmanuel <ayrtonmanuel@gmail.com>
2025-01-07 05:33:49 -0400

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,10 +1,9 @@
-import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
-
-// A list of Warp Route token configs
-// These configs will be merged with the warp routes in the configured registry
-// The input here is typically the output of the Hyperlane CLI warp deploy command
-export const warpRouteConfigs: WarpCoreConfig = {
-   tokens:
+# A list of Warp Route token configs
+# These configs will be merged with the warp routes in the configured registry
+# The input here is typically the output of the Hyperlane CLI warp deploy command
+---
+ tokens: [
+     {
       - chainName: base
         standard: EvmHypNative
         decimals: 18
@@ -16,6 +15,8 @@ export const warpRouteConfigs: WarpCoreConfig = {
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
           - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
+            },
+    {
       - chainName: lisk
         standard: EvmHypNative
         decimals: 18
@@ -27,6 +28,8 @@ export const warpRouteConfigs: WarpCoreConfig = {
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
           - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
+            },
+    {
       - chainName: mint
         standard: EvmHypNative
         decimals: 18
@@ -38,6 +41,8 @@ export const warpRouteConfigs: WarpCoreConfig = {
           - token: ethereum|lisk|0x274c9f6ac5bD2Be638e8122e8C656a9899B60cc8
           - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
+            },
+    {
       - chainName: optimism
         standard: EvmHypNative
         decimals: 18
@@ -49,6 +54,8 @@ export const warpRouteConfigs: WarpCoreConfig = {
           - token: ethereum|lisk|0x274c9f6ac5bD2Be638e8122e8C656a9899B60cc8
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
           - token: ethereum|swell|0x174832329F460322C8D8A417b22c72a5088f1d96
+            },
+    {
       - chainName: swell
         standard: EvmHypCollateral
         decimals: 18
@@ -60,6 +67,7 @@ export const warpRouteConfigs: WarpCoreConfig = {
           - token: ethereum|base|0xF823f5b1E4017190c14c4cf975DF0d33e5AF6A40
           - token: ethereum|lisk|0x274c9f6ac5bD2Be638e8122e8C656a9899B60cc8
           - token: ethereum|mint|0x73e95f62CaD1624f93142aEd3A8045C230bAc3bd
-          - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C,
-  options: {},
-};
+          - token: ethereum|optimism|0xE8D11E04Cf6bE23CB8c6aeC0a52ddA2Ac586dD9C
+    }
+]
+options: {}
