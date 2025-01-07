d45f64b0d081994173bc61e722504c6fac3d635f
Ayrtonmanuel <ayrtonmanuel@gmail.com>
2025-01-07 05:41:10 -0400

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,7 +1,3 @@
-# A list of Warp Route token configs
-# These configs will be merged with the warp routes in the configured registry
-# The input here is typically the output of the Hyperlane CLI warp deploy command
----
  tokens: [
      {
       - chainName: base
